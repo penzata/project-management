51611d6a59ac19d3d91c0e486644b42d60106c90
penzata <penzata86@yahoo.com>
2023-03-09 14:11:48 +0200

    multi-model maven & pom config

---
+++ app/src/main/java/org/project/management/app/ProjectManagementApplication.java
@@ -0,0 +1,12 @@
+package org.project.management.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProjectManagementApplication.class, args);
+    }
+}
